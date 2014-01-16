49f4eb9362e6aded75ec8e3f7efa35013ae267ce
koti <koti@ekanna.com>
2014-01-16 13:31:31 +0530

    multi column barchart

--- articles/rbarchart.js
+++ articles/rbarchart.js
@@ -1,10 +1,30 @@
 "use strict"
-// Column Bar Chart
+/*
+var bdata = {
+	latest: [{label: 'jan', values: [125,145]},{label: 'feb', values: [143,153]},{label: 'mar', values: [170,180]}, {label: 'apr', values: [150,160]}, {label: 'may', values: [160,170]},
+		{label: 'jun', values: [185,200]}]
+}
+
+var bdata = {
+	latest: [{label: 'jan', values: [145]},{label: 'feb', values: [153]},{label: 'mar', values: [180]}, {label: 'apr', values: [160]}, {label: 'may', values: [170]},
+		{label: 'jun', values: [200]}]
+}
+*/
+
+var bdata = {
+	latest: [{label: 'jan', values: [125,145,135]},{label: 'feb', values: [143,153,160]},{label: 'mar', values: [170,180,185]}, {label: 'apr', values: [150,160,180]}, {label: 'may', values: [160,170,180]},
+		{label: 'jun', values: [185,200,250]}],
+	old: [{label: 'jan', values: [150,145,135]},{label: 'feb', values: [143,153,160]},{label: 'mar', values: [170,180,185]}, {label: 'apr', values: [150,160,180]}, {label: 'may', values: [160,170,180]},
+		{label: 'jun', values: [185,200,300]}]
+}
+
+// Single Or Multi Column Bar Chart
 var BarChart = Ractive.extend({
 	template: '#barChartT',
 	init: function(options) {
 		var self = this;
 		this.bdata = options.bdata;
+		this.legend = options.legend;
 		this.drawChart(this.bdata.latest)
 		this.on({
 			lastyear: function(event){
@@ -21,13 +41,19 @@ var BarChart = Ractive.extend({
 		return svgWidth/(data.length * 2) * 0.9;
 	},
 	dataScale: function(data, svgHeight){
-		var inputMax = this.max(this.pluck(data, 'value'));
+		var inputMax = this.max(this.merge(this.pluck(data, 'values')));
 		var output = [];
 		var scale = this.linearScale({input:[0, inputMax], output:[0, svgHeight * 0.7]})
 		data.forEach(function(obj){
-			obj.height = scale(obj.value);
+			obj.height = []
+			obj.count = obj.values.length;
+			console.log(obj.count)
+			obj.values.forEach(function(item, i){
+				obj.height.push(scale(item))
+			});
 			output.push(obj)
 		})
+		console.log(output)
 		return output;
 	},
 	linearScale: function(obj){
@@ -37,6 +63,16 @@ var BarChart = Ractive.extend({
 		return function(value){
 			return outputRange[0] + (value - inputDomain[0]) * factor;
 		};
+	},
+	// arrOfArrs = [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
+	merge: function(arrOfArrs){
+		var output = [];
+		arrOfArrs.forEach(function(arr){
+			arr.forEach(function(item){
+				output.push(item)
+			});
+		});
+		return output;
 	},
 	max: function(arr){
 		return Math.max.apply(null, arr);
@@ -57,20 +93,18 @@ var BarChart = Ractive.extend({
 			svgHeight: svgHeight,
 			mydata: self.dataScale(data, svgHeight),
 			width: self.barWidth(data, svgWidth),
+			legend: self.legend,
 		});
 	}
 });
 
-var bdata = {
-	latest: [{label: 'jan', value: 145},{label: 'feb', value: 153},{label: 'mar', value: 180}, {label: 'apr', value: 160}, {label: 'may', value: 170},
-		{label: 'jun', value: 200}, {label: 'jul', value: 210}, {label:'aug', value: 220}, {label: 'sep', value: 210}, {label: 'oct', value: 240}, {label: 'nov', value:200}, {label: 'dec', value: 250}],
-	old: [{label: 'jan', value: 130},{label: 'feb', value: 153},{label: 'mar', value: 180}, {label: 'apr', value: 160}, {label: 'may', value: 170},
-		{label: 'jun', value: 200}, {label: 'jul', value: 210}, {label:'aug', value: 220}, {label: 'sep', value: 210}, {label: 'oct', value: 240}, {label: 'nov', value:200}, {label: 'dec', value: 150}],
-}
-
 var barChart = new BarChart({
 	el: '#container',
 	bdata: bdata,
+	legend: {
+		colors:['green', 'blue', 'yellow'],
+		colorLables: ['Chemestry', 'Physics', 'Maths'],
+	},
 });
 var resize = function(){
 	barChart.drawChart(barChart.bdata.latest);
